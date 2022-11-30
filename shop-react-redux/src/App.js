c39ee0c8f0ac1b5b7f7729c30f83de268781f867
k <kosarghasem@gmail.com>
2022-11-30 23:02:53 +0330

    fixed add source code in project

--- shop-react-redux/src/App.js
+++ shop-react-redux/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navigate, Route, Routes } from "react-router-dom";
 
-function App() {
+// MUI
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./frontEnd/mui/theme";
+// Redux
+import { Provider } from "react-redux";
+import { store } from "./frontEnd/redux/store";
+// Components
+import Products from "./frontEnd/components/Products";
+import CartContainer from "./frontEnd/components/cart/CartContainer";
+import ProductDetails from "./frontEnd/components/product/ProductDetails";
+
+function AppContainer() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/cart" element={<CartContainer />} />
+          <Route path="/Products/:id" element={<ProductDetails />} />
+
+          <Route path="/" element={<Navigate to="/products" />} />
+        </Routes>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
-export default App;
+export default AppContainer;
---
+++ shop-react-redux/src/frontEnd/data/index.js
@@ -0,0 +1,111 @@
+export const Data = [
+  {
+    id: 1,
+    price: "200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/06de51dfd84a099ed3f8ae19b5abad601c119272_1653201178.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "مبل راحتی 8 نفره",
+    quantity: 1,
+    faDescription: "مبل راحتی  یک محصول جدید با طراحی متفاوت و خاص است. این مبل ظاهر بسیار جذابی دارد و می‌تواند دکوراسیون منزل شما را خاص و چشم نواز کند. به دلیل طراحی جالب و منحصر به فرد مبل  از آن در دکوراسیون های اداری و تجاری نیز می توان استفاده کرد.",
+  },
+  {
+    id: 2,
+    price: "4260000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/59994a18b29c17bcbc926d07490a3d943b1de1f4_1599425563.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "تابلو مدل GS215",
+    quantity: 1,
+    faDescription: "تابلو پازلی چند تکه لوکس از سری تابلوهای مدرن با طرح های جذاب و هارمونیک که به راحتی یک دیوار معنا و روح می بخشند. این تابلو زیبا با قاب pvc و چاپ لابراتوار در ابعاد شصت در چهل می باشد.",
+  },
+  {
+    id: 3,
+    price: "5200000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/127be630f1c01015fc47b13bc1b525af1f2c61f7_1605452071.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "کتابخانه",
+    quantity: 1,
+    faDescription: "کتابخانه های چوبی معمولا از انواع مختلف چوب مانند گردو، راش، روس، چنار و … تهیه می شوندو بسته به کیفیت و  نوع چوب معمولا از قیمت های بالایی برخوردارند. همچنین بیشتر در سبک های بوهو، کلاسیک و … استفاده می شوند. زیرا بیشتر حس طبیعت و نوستالژی را القا می کنند",
+  },
+  {
+    id: 4,
+    price: "6800000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/dbd0411964021abb03f46b5e9c709cc24dbdd8e6_1644826635.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "کتابخانه آراسک",
+    quantity: 1,
+    faDescription: "این مدل کتابخانه از یکسری مکعب های عمودی یا افقی که با یک دیگر ترکیب شده اند طراحی میشوند. گاهی علاوه بر اینکه از مکعب ها برای نگهداری کتاب ها استفاده شود از آن ها برای نگهداری لباس، کفش و... نیز استفاده میشود با این تفاوت که برای مکعب ها درپوشی را طراحی می کنند.",
+  },
+  {
+    id: 5,
+    price: "57800000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/05c1158b5e24d248583c1c7ac2a00b4c606d9226_1642417302.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "پرده مدل هازان چاپی طرح گل",
+    quantity: 1,
+    faDescription: "پرده چاپی از انواع مدل های تزیینی و خاص می باشد و اگر شما تمایل دارید تغییرات مثبتی را در طراحی دکوراسیون داخلی قسمت های مختلف منزل خود ایجاد نمایید بهتر است طرحی مناسب از این نوع پرده را انتخاب نمایید.",
+  },
+  {
+    id: 6,
+    price: "9500000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/ad1a1b21f0d1aa0e6df05a820cd2b1bf368ece74_1650650841.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "تابلو مدل برگ",
+    quantity: 1,
+    faDescription: "تابلو مدرن سه تکه از سری تابلوهای مدرن با طرح های جذاب و هارمونیک که به راحتی یک دیوار معنا و روح می بخشند. این 3 تابلو زیبا با قاب pvc و چاپ لابراتوار قابل سفارش در 2 سایز متوسط 30*40 و بزرگ 60*40 می باشند.",
+  },
+  {
+    id: 7,
+    price: "39000000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/126db5d49ba46554d8954e44ced921afbde338a9_1637550791.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "لوستر مدرن",
+    quantity: 1,
+    faDescription: "لوسترهای مدرن در حال حاضر بسیار محبوب هستند چرا که مناسب دکوراسیون مدرن امروزی می‌باشند. این مدل طراحی نامحدودی دارد و هر روز نیز بر تعداد طرح‌های متنوع این لوستر افزوده می‌شود",
+  },
+  {
+    id: 8,
+    price: "7480000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/ff13e46487118e3d55c3375c067c1dc7d56c9b2a_1619380184.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "تابلو مدل برگ انجیری",
+    quantity: 1,
+    faDescription: "تابلو پازلی 3 تکه طرح یاس از سری تابلوهای مدرن با طرح های جذاب و هارمونیک که به راحتی یک دیوار معنا و روح می بخشند. این 3 تابلو زیبا با قاب pvc و چاپ لابراتوار قابل سفارش در 2 سایز متوسط 30*40 و بزرگ 60*40 می باشند.",
+  },
+  {
+    id: 9,
+    price: "58300000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/a21b3567e86dd5ca94da67ddec230b9a85e682eb_1609758344.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "پرده دکوریما",
+    quantity: 1,
+    faDescription: "این مدل پرده  از انواع طرح های خاص و مدرن پرده است که در قسمت زیری آن از آستر استفاده نمی شود و اگر شما تمایل دارید پنجره ها از قسمت داخلی و خارجی منزل نمایی زیبا و بی نظیر داشته باشند بهتر است از این طرح استفاده نمایید",
+  },
+
+  {
+    id: 10,
+    price: "35700000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/e1ca2d42f0f71b0974a64b457cf080ade39f8925_1625205311.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "پرده نیروانا",
+    quantity: 1,
+    faDescription: " این مدل پرده از انواع پرده ها با طرح شیک و بی نظیر می باشد و اگر شما تمایل دارید در طراحی دکوراسیون داخلی منزل تان تغییرات مثبتی را انجام دهید بهتر است این مدل پرده را برای نصب روی پنجره ها انتخاب کنید.",
+  },
+  {
+    id: 11,
+    price: "6500000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/2fb32c6affa1d007066640ee98910972062036bb_1627971675.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "میز کامپیوتر",
+    quantity: 1,
+    faDescription: "میز کامپیوتر چوبی یا ام دی اف، به نوعی از میز اطلاق می شود که از چوب طبیعی درختان به ویژه درخت کاج تهیه می شود. برخی انواع این میزها دارای پایه هایی است که به سادگی توسط پیچ و میخ بر روی پایه‌ها نصب می شود و همین مسیله، ظاهر آنها را در مقایسه با سایر میزها زیباتر می کند. میز کامپیوتر های ام دی اف یکی از نمونه های زیبایی هستند که طراحی ساده و در عین حال شیکی دارند و در دو نوع گرد و مستطیل موجود هستند.",
+  },
+  {
+    id: 12,
+    price: "4260000",
+    image:
+      "https://dkstatics-public.digikala.com/digikala-products/1078971.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+    faTitle: "ساعت دیواری",
+    quantity: 1,
+    faDescription: "ساعت دیواری یکی از وسایل کاربردی و از لحاظ تزئینی نقش بسیار مهمی را در دکوراسیون و زیبایی منزل ما ایفا می کند و از گذشته تا به امروز دست خوش تغییرات زیادی از لحاظ ساختاری و شکل ظاهری شده است ",
+  },
+];
